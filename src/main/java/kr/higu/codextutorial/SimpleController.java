11fdcd695ba0894bb7433efc2b3019874c817c42
heegu123 <kookminkhg@kookmin.ac.kr>
2026-03-26 18:57:47 +0900

    first commit

---
+++ src/main/java/kr/higu/codextutorial/SimpleController.java
@@ -0,0 +1,16 @@
+package kr.higu.codextutorial;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/simple")
+public class SimpleController {
+
+    @GetMapping("/{name}")
+    public String displayName(@PathVariable String name) {
+        return "hi " + name;
+    }
+}
